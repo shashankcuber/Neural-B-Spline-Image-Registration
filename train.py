9fb0dd1d044232c390c278f4eccd5da3e48bcf18
Shashank <shashankjag3@gmail.com>
2024-12-11 18:05:39 -0700

    Option added for both the dataset

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ import wandb
 import matplotlib.pyplot as plt
 import pickle
 
-wandb.login(key = "")
+wandb.login(key = '<PASTE YOUR WANDB KEY>')
 
 class Dataset(data.Dataset):
     """
@@ -23,9 +23,10 @@ class Dataset(data.Dataset):
     """
     'Characterizes a dataset for PyTorch'
 
-    def __init__(self, list_IDs):
+    def __init__(self, list_IDs, data_type="segmented"):
         'Initialization'
         self.list_IDs = list_IDs
+        self.data_type = data_type
 
     def __len__(self):
         'Denotes the total number of samples'
@@ -37,22 +38,28 @@ class Dataset(data.Dataset):
         ID = self.list_IDs[index]
 
         # Load data and get label
-        fixed_image = torch.Tensor(
-            resize(io.imread('./fire-fundus-image-registration-dataset/' + ID + '_1.jpg'), (256, 256, 3)))
-        moving_image = torch.Tensor(
-            resize(io.imread('./fire-fundus-image-registration-dataset/' + ID + '_2.jpg'), (256, 256, 3)))
-        return fixed_image, moving_image
+        seg_path = './FUNDUS_seg_output/'
+        fixed_image = torch.Tensor( resize(io.imread(seg_path + ID + '_1.png'), (256, 256, 1)))
+        moving_image = torch.Tensor( resize(io.imread(seg_path + ID + '_2.png'), (256, 256, 1)))
+
+
+        if self.data_type != "segmented":
+            fixed_image = torch.Tensor(
+                resize(io.imread('./Fire/' + ID + '_1.jpg'), (256, 256, 3)))
+            moving_image = torch.Tensor(
+                resize(io.imread('./Fire/' + ID + '_2.jpg'), (256, 256, 3)))
+        
+        return fixed_image, moving_image, ID
     
 
-class Registration_Net(nn.Module):
+class Registration_Net():
     def __init__(self, input_channels, output_channels, train_config):
-        super(Registration_Net, self).__init__()
         self.nb = nb
         self.train_config = train_config
         self.input_channels = input_channels
         self.output_channels = output_channels
         self.device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
-        self.model = nb.IRnet(image_size=(3,256,256), num_points=train_config['num_points'], num_patches=train_config['num_patches'], device=self.device).to(self.device)
+        self.model = nb.IRnet(image_size=(1, 256, 256), num_points=train_config['num_points'], num_patches=train_config['num_patches'], device=self.device).to(self.device)
 
         self.optimizer = torch.optim.SGD(self.model.parameters(), lr=train_config['lr'], momentum=train_config['momentum'])
         # self.optimizer = torch.optim.Adam(self.model.parameters(), lr=train_config['lr'])
@@ -134,7 +141,7 @@ class Registration_Net(nn.Module):
         
         return train_loss, dice_score
     
-    def get_test_loss(self, batch_moving, batch_fixed, epoch, count):
+    def get_test_loss(self, batch_moving, batch_fixed, epoch, count, ID):
         with torch.no_grad():
             batch_moving, batch_fixed = batch_moving.to(self.device), batch_fixed.to(self.device)
             dense_field = self.model(batch_moving, batch_fixed)
@@ -149,7 +156,7 @@ class Registration_Net(nn.Module):
 
             val_dice_score = self.nb.dice_score(registered_image.permute(0, 2, 3, 1), batch_fixed)
             
-            if count == 0:
+            if count == 1 and epoch % 10 == 0:
                 nb.visualise_results(batch_fixed[0], batch_moving[0], registered_image[0].permute(1, 2, 0), xy, xyd_[0], epoch, self.train_config['num_points'])
             
             # nb.visualise_results(batch_fixed[0], batch_moving[0], registered_image[0], xy_, dense_field[0], epoch)
@@ -158,74 +165,77 @@ class Registration_Net(nn.Module):
 
 def main():
 
-    # DATA_PATH = './fire-fundus-image-registration-dataset/'
+    train_config = {
+        "epochs":100,
+        "batch_size": 4,
+        "lr": 1e-5,
+        "momentum": 0.99,
+        "num_points": 50,
+        "num_patches": 32,
+        "device": "cuda:1",
+        "train_size": len(training_generator),
+        "val_size": len(validation_generator),
+        "data_type": "segmented",
+    }
+
     params = {'batch_size': 1,
-                'shuffle': True,
+                'shuffle': False,
                 'num_workers': 2,
                 'worker_init_fn': np.random.seed(42)
                 }
     
-    # filename = list(set([x.split('_')[0] for x in os.listdir('./fire-fundus-image-registration-dataset/')]))
+    filename = list(set([x.split('_')[0] for x in os.listdir('./Fire-segmented/')]))
 
-    # print(len(filename))
+    print(len(filename))
 
-    # partition = {}
+    partition = {}
 
-    # partition['train'], partition['validation'] = train_test_split( filename, test_size=0.1, random_state=42)
+    # Storing the train and validation data file names for reproducibility
+    partition['train'], partition['validation'] = train_test_split( filename, test_size=0.1, random_state=42)
 
-    # with open('partition.pkl', 'wb') as f:
-    #     pickle.dump(partition, f)
+    with open('partition.pkl', 'wb') as f:
+        pickle.dump(partition, f)
     
-    # exit()
     with open('partition.pkl', 'rb') as f:
         partition = pickle.load(f)
 
     print(len(partition['train']))
     print(len(partition['validation']))
     # Generators
-    training_set = Dataset(partition['train'])
+    training_set = Dataset(partition['train'], data_type="segmented")
     training_generator = data.DataLoader(training_set, **params)
 
-    validation_set = Dataset(partition['validation'])
+    validation_set = Dataset(partition['validation'], data_type="segmented")
     validation_generator = data.DataLoader(validation_set, batch_size=1, shuffle=False)
 
-    # print(len(training_generator))
-    # print(len(validation_generator))
-
-    train_config = {
-        "epochs": 10,
-        "batch_size": 4,
-        "lr": 1e-4,
-        "momentum": 0.99,
-        "num_points": 10,
-        "num_patches": 32,
-        "device": "cuda:1",
-        "train_size": len(training_generator),
-        "val_size": len(validation_generator)
-    }
-
     rnet = Registration_Net(input_channels=3, output_channels=2, train_config=train_config)
+
+    # Turn it to True to test the model
+    test = False
+    if test == True:
+        train_config["epochs"] = 1
 
     with wandb.init(project="Neural-B-spline-Image-Registration", config=train_config, name=f"Neural-Bspline-{train_config['num_points']}-num_points") as run:
         min_val_loss = float('inf')
         for epoch in range(train_config["epochs"]):
-            start_time = time.time()
-            train_loss = 0
-            train_dice_score = 0
-            val_loss = 0
-            val_dice_score = 0
-            for batch_fixed, batch_moving in training_generator:
-                loss, dice = rnet.train_model(batch_moving, batch_fixed)
-                train_dice_score += dice.data
-                train_loss += loss.data
-            avg_train_loss = train_loss / len(training_generator)
-            avg_train_dice_score = train_dice_score / len(training_generator)
-            print("Epoch: ", epoch, "Train Loss: ", avg_train_loss, "Train Dice Score: ", avg_train_dice_score)
+            if test == False:
+                start_time = time.time()
+                train_loss = 0
+                train_dice_score = 0
+                val_loss = 0
+                val_dice_score = 0
+                for batch_fixed, batch_moving, ID in training_generator:
+                    loss, dice = rnet.train_model(batch_moving, batch_fixed)
+                    train_dice_score += dice.data
+                    train_loss += loss.data
+                avg_train_loss = train_loss / len(training_generator)
+                avg_train_dice_score = train_dice_score / len(training_generator)
+                print("Epoch: ", epoch, "Train Loss: ", avg_train_loss, "Train Dice Score: ", avg_train_dice_score)
 
             start_time = time.time()
             count = 0
-            for batch_fixed, batch_moving in validation_generator:
-                loss, dice = rnet.get_test_loss(batch_moving, batch_fixed, epoch, count)
+            for batch_fixed, batch_moving, ID in validation_generator:
+                loss, dice = rnet.get_test_loss(batch_moving, batch_fixed, epoch, count, ID)
                 count += 1
                 val_dice_score += dice.data
                 val_loss += loss.data
@@ -234,7 +244,7 @@ def main():
             avg_val_dice_score = val_dice_score / len(validation_set)
             if avg_val_loss < min_val_loss:
                 min_val_loss = avg_val_loss
-                torch.save(rnet.model.state_dict(), f"./best_model/best_model_{train_config['num_points']}_num_points.pth")
+                # torch.save(rnet.model.state_dict(), f"./best_model/best_model_{train_config['num_points']}_num_points.pth")
 
             print("Epoch: ", epoch, "Validation Loss: ", avg_val_loss, "Validation Dice Score: ", avg_val_dice_score)
             wandb.log(
